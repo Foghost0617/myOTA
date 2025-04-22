38a81f9d6950b1032b38343cbfa1bdc16e99da18
yhy <15665917+foghost0617@user.noreply.gitee.com>
2025-04-22 12:47:21 +0800

    补充误删的userschema

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -2,6 +2,49 @@ from pydantic import BaseModel
 from typing import Optional
 
 
+# 请求体：注册时用到的字段
+class UserRegister(BaseModel):
+    account: str
+    password: str
+    role: int
+
+    class Config:
+        from_attributes = True  # 允许将 Pydantic 模型转换为 ORM 模型
+
+
+
+# 注册返回的字段
+class UserOut(BaseModel):
+    id: int
+    account: str
+    role: int
+
+    class Config:
+        from_attributes = True
+
+
+
+# 请求体：登录时用到的字段
+class UserLogin(BaseModel):
+    account: str
+    password: str
+    role: int
+
+
+
+class UserLoginOut(BaseModel):
+    id: int
+    account: str
+    role: int
+
+    # 各角色的ID（可选）
+    agency_id: Optional[int] = None
+    guide_id: Optional[int] = None
+    tourist_id: Optional[int] = None
+    gov_id: Optional[int] = None
+
+    class Config:
+        from_attributes = True
 
 
 class GuideUpdate(BaseModel):
@@ -19,6 +62,7 @@ class GuideOut(BaseModel):
 
     class Config:
         from_attributes = True
+
 
 # 获取所有旅社？
 class TravelAgencyOut(BaseModel):
@@ -45,3 +89,7 @@ class TouristBasic(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+
+
